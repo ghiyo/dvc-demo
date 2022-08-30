3a566e7e260a0024de10a2839ef07c75904eb655
Omid Ghiyasian <o.ghiyasi@gmail.com>
2022-08-29 21:13:27 -0400

    retesting

--- python-script.py
+++ python-script.py
@@ -6,7 +6,7 @@ import os
 def read_file(filename):
     """Reads and returns the contents of a file in a list of strings"""
     cur_path = os.path.dirname(__file__)
-    new_path = os.path.relpath(f"../input/{filename}", cur_path)
+    new_path = os.path.relpath(f"./data/{filename}", cur_path)
     f = open(new_path, "r", encoding="utf-8")
     lines = [i.strip() for i in f.readlines()]
     f.close()
@@ -15,9 +15,10 @@ def read_file(filename):
 
 def main():
     """Main function"""
-    raw_input = read_file("test.txt")
+    raw_input = read_file("data.txt")
     for i in raw_input:
-        print(i)
+        print(i, end=" ")
+    print()
 
 
 if __name__ == "__main__":
